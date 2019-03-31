9b74341472693593e1c0ac0cfbcb331f1d4c1ab1
Boobalan <boobalanmaker@gmail.com>
2019-03-31 21:10:14 +0530

    universal interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { ServerTransferStateModule } from '@angular/platform-server';
+import { UniversalInterceptor } from '../interceptors/universal.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -13,6 +15,13 @@ import { ServerTransferStateModule } from '@angular/platform-server';
     ModuleMapLoaderModule,
     ServerTransferStateModule
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UniversalInterceptor,
+      multi: true
+    }
+  ]
 })
 export class AppServerModule {}
--- src/providers/hero.service.ts
+++ src/providers/hero.service.ts
@@ -18,7 +18,7 @@ const httpOptions = {
 
 @Injectable()
 export class HeroService {
-  private heroesUrl = 'http://localhost:4000/api/heroes'; // URL to web api
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   constructor(
     private http: HttpClient,
