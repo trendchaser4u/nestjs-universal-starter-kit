0c8e518a24d057a15bb5f62a2822f2d5a8c9e20c
boobalan <boobalan.muthukumaran@biworldwide.com>
2019-04-01 14:39:53 +0530

    dev server webpack

--- webpack.dev.server.config.js
+++ webpack.dev.server.config.js
@@ -3,30 +3,45 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+  // entry: {
+  //   app: './server/main.ts'
+  // },
   entry: ['webpack/hot/poll?100', './server/main.ts'],
   watch: true,
+  mode: 'none',
   target: 'node',
+  resolve: { extensions: ['.tsx', '.ts', '.js', '.json'] },
   externals: [
     nodeExternals({
-      whitelist: ['webpack/hot/poll?100']
+      whitelist: [
+        'webpack/hot/poll?100',
+        /^(?!@nestjs\/(common|core|microservices)).*/
+      ]
     })
   ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  },
   module: {
     rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
       {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: ['ts-loader']
+        // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
+        // Removing this will cause deprecation warnings to appear.
+        test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
+        parser: { system: true }
       }
     ]
   },
-  mode: 'none',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.json']
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'server.js'
-  }
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+    new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
+      /((.+)?angular(\\|\/)core(.+)?|express(.+)?|(.+)?nestjs(\\|\/)(.+)?)?/,
+      path.join(__dirname, 'src'), // location of your src
+      {} // a map of your routes
+    )
+  ]
 };
